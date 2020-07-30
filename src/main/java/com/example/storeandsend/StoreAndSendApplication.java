b3136b503c771b6cdcd0c288a06fe33dca278a2a
DiShc <shvetsda@gmail.com>
2020-07-30 17:41:28 +0300

    Initial commit

---
+++ src/main/java/com/example/storeandsend/StoreAndSendApplication.java
@@ -0,0 +1,13 @@
+package com.example.storeandsend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreAndSendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreAndSendApplication.class, args);
+	}
+
+}
